5288321d24336df20edbfa7d337fa4fb4501cdf1
nyimpe <nyimpe52@gmail.com>
2023-11-18 16:20:19 +0900

    markdown syntax hightlight

--- src/components/MDRenderer.jsx
+++ src/components/MDRenderer.jsx
@@ -1,8 +1,23 @@
 import { useEffect, useState } from "react";
-import { load } from "js-yaml";
+import { Container } from "@mui/material";
 
 import Markdown from "react-markdown";
-import item from "../posts/2023-11-12-First.md";
+import { load } from "js-yaml";
+
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("markdown", markdown);
+SyntaxHighlighter.registerLanguage("json", json);
+
+// import item from "../posts/2023-11-12-First.md";
+import item from "../posts/README.md";
 
 const MDRenderer = () => {
   const [content, setContent] = useState("");
@@ -15,11 +30,13 @@ const MDRenderer = () => {
       //   console.log(filePath);
     });
   };
+
   useEffect(() => {
     const frontMatterRegex = /^---\n([\s\S]*?)\n---/;
     const match = content.match(frontMatterRegex);
     if (match && match[1]) {
-      console.log("match->", load(match[1]));
+      console.log("match->", match[1]);
+      // console.log("match->", load(match[1]));
     }
     getPostList();
   }, [content]);
@@ -30,7 +47,31 @@ const MDRenderer = () => {
       .then((text) => setContent(text));
   }, []);
 
-  return <Markdown>{content}</Markdown>;
+  return (
+    <Container fixed>
+      <Markdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                PreTag="div"
+                {...props}
+                style={materialDark}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props}>{children}</code>
+            );
+          },
+        }}
+      >
+        {content}
+      </Markdown>
+    </Container>
+  );
 };
 
 export default MDRenderer;
